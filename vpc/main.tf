39176f460971dcfbfb25d52a7a7f591a7cadcc22
Ryan Tiffany <greyhoundforty@users.noreply.github.com>
2022-07-12 14:14:31 -0500

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -3,7 +3,7 @@
 resource "ibm_is_vpc" "vpc" {
   name           = "${var.name}-vpc"
   resource_group = data.ibm_resource_group.group.id
-  tags           = concat(var.tags, ["region:${var.region}", "vpc:${var.name}-vpc"])
+  tags           = concat(var.tags, ["region:${var.vpc_region}", "vpc:${var.name}-vpc"])
 }
 
 resource "ibm_is_public_gateway" "zone_1" {
@@ -11,7 +11,7 @@ resource "ibm_is_public_gateway" "zone_1" {
   vpc            = ibm_is_vpc.vpc.id
   zone           = data.ibm_is_zones.region.zones[0]
   resource_group = data.ibm_resource_group.group.id
-  tags           = concat(var.tags, ["region:${var.region}", "zone:${data.ibm_is_zones.region.zones[0]}", "vpc:${var.name}-vpc"])
+  tags           = concat(var.tags, ["region:${var.vpc_region}", "zone:${data.ibm_is_zones.region.zones[0]}", "vpc:${var.name}-vpc"])
 }
 
 resource "ibm_is_subnet" "subnet" {
@@ -22,5 +22,5 @@ resource "ibm_is_subnet" "subnet" {
   total_ipv4_address_count = "64"
   resource_group           = data.ibm_resource_group.group.id
   public_gateway           = ibm_is_public_gateway.zone_1.id
-  tags                     = concat(var.tags, ["region:${var.region}", "zone:${data.ibm_is_zones.region.zones[count.index]}", "vpc:${var.name}-vpc"])
+  tags                     = concat(var.tags, ["region:${var.vpc_region}", "zone:${data.ibm_is_zones.region.zones[count.index]}", "vpc:${var.name}-vpc"])
 }
